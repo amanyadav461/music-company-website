4bee9cffb12a4d685f6b451081f7dbd731b59b14
amanyadav461 <68475737+amanyadav461@users.noreply.github.com>
2021-11-26 14:02:14 +0530

    v1 for git

---
+++ frontend/src/components/Header/HeaderSliderDots.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const SliderDots = styled("div")`
+	li.slick-active button {
+		color: white;
+	}
+
+	.slick-dots li button {
+		color: red;
+	}
+`;
+
+export default SliderDots;
